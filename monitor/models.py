3b1d2d7529fdfb97f7f05a7c3814e8fa73f35b5d
OTR <49165533+OTR@users.noreply.github.com>
2021-04-24 12:02:51 +0300

    Add url routing to an App

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class TempVolModel(models.Model):
+	temp_intake = models.IntegerField()
+
+	def __str__(self):
+		return "ЦВУ "
--- monitor/views.py
+++ monitor/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView, View
+from monitor.models import TempVolModel
 # Create your views here.
+
+#class IndexView(DetailView):
+#	model = TempVolModel
+class IndexView(View):
